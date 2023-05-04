1d148a86e6e7d8d95cd4ef06b0f4e1098092c4c6
Xander <xandervoorvaart@protonmail.com>
2023-05-04 15:47:44 +0200

    Changed session-formatter & video is now summarized

--- session-formatter/src/main.py
+++ session-formatter/src/main.py
@@ -1,26 +1,30 @@
-from typing import Any, List
-from session import Session
-from gspread import Worksheet
-from consts import PRIMARY_KEYWORDS
-from parse_session import parse_sessions
-from auth_google_account import open_sheet
+import sys
+import json
+import subprocess
+from typing import Any
 
 
-def get_cell_data(row: list):
-    matched_columns = []
+def open_session(session_path: str) -> Any:
+    with open(session_path) as file:
+        data = json.load(file)
 
-    for index, cell in enumerate(row):
-        if cell in PRIMARY_KEYWORDS:
-            column_letter = chr(index + ord('A'))
-            matched_columns.append((cell, column_letter))
+    return data
 
-    return matched_columns
+
+def main() -> None:
+    json_file = open_session(sys.argv[1])
+
+    if 'video' in json_file:
+        print('Summarizing video...')
+        subprocess.run(['python3', './summarize-video_to_text/src/main.py', json_file['video'], 'test'])
+    else:
+        print('Video does not exist in file')
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    worksheet: Worksheet = open_sheet()
-    filled_columns: list[tuple[Any, str]] = get_cell_data(worksheet.row_values(1))
+    if len(sys.argv) != 2:
+        print('Need an argument to a JSON file')
+        sys.exit(1)
 
-    # Check if it exists and if it is "Session"
-    if filled_columns[2] and filled_columns[2][0] == PRIMARY_KEYWORDS[2]:
-        all_sessions: List[Session] = parse_sessions(worksheet, filled_columns[2])
+    main()
--- summarize-video_to_text/src/main.py
+++ summarize-video_to_text/src/main.py
@@ -1,9 +1,51 @@
 import argparse
+import os
 import subprocess
+import sys
 import whisper
+import openai
+from os.path import dirname, join
+from dotenv import load_dotenv
 
 
-def download_m3u8_video_as_wav(m3u8_url: str, output_wav: str) -> None:
+def process_text_file(transcribed_text: str) -> str:
+    content = transcribed_text.replace('um', '').replace('uh', '').replace(',', '').replace('  ', ' ')
+
+    words = content.split()
+    word_count = len(words)
+
+    if word_count > 1700:
+        print('Content too long, will trim file')
+        words = words[:1700]
+        content = ' '.join(words)
+
+    return content
+
+
+def summarize_text(input_text: str, output_file: str) -> None:
+    openai.api_key = os.environ.get('CHATGPT_API_KEY')
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": f"Summarize this for a description for a video:\n\n{input_text}"}
+        ],
+        temperature=0.4,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    summary = response.choices[0].message["content"].strip()
+    if os.path.exists(output_file):
+        with open(output_file, 'w') as file:
+            file.write(summary)
+    else:
+        with open(output_file, 'x') as file:
+            file.write(summary)
+
+
+def download_m3u8_video_as_wav(m3u8_url: str, output_name: str) -> None:
     command = [
         "ffmpeg",
         "-i", m3u8_url,
@@ -11,38 +53,41 @@ def download_m3u8_video_as_wav(m3u8_url: str, output_wav: str) -> None:
         "-c:a", "pcm_s16le",
         "-ar", "44100",
         "-ac", "2",
-        output_wav
+        f'{output_name}.wav'
     ]
 
     try:
         subprocess.run(command, check=True)
-        print(f"Successfully downloaded and converted audio from {m3u8_url} to {output_wav}")
+        print(f"Successfully downloaded and converted audio from {m3u8_url} to {output_name}.wav")
     except subprocess.CalledProcessError as e:
-        print(f"Error occurred while downloading and converting audio from {m3u8_url} to {output_wav}: {e}")
+        print(f"Error occurred while downloading and converting audio from {m3u8_url} to {output_name}.wav: {e}")
 
 
-def convert_speech_to_text(output_wav: str) -> None:
+def convert_speech_to_text(input_wav: str) -> str:
     model = whisper.load_model("base")
-    result = model.transcribe(output_wav)
+    result = model.transcribe(input_wav)
     text = result["text"]
 
-    with open("output.txt", 'w') as file:
-        file.write(text)
-
     print(f"Successfully saved speech-to-text result")
+    return text
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description="Download audio from an M3U8 URL and save it as a WAV file using ffmpeg")
+        description="Download audio from an M3U8 URL, convert it to WAV, transcribe it, trim, and summarize")
     parser.add_argument("m3u8_url", help="URL of the M3U8 file")
-    parser.add_argument("output_wav", help="Path to the output WAV file")
+    parser.add_argument("output_name", help="Path to the output WAV file")
 
     args = parser.parse_args()
 
-    download_m3u8_video_as_wav(args.m3u8_url, args.output_wav)
-    convert_speech_to_text(args.output_wav)
+    download_m3u8_video_as_wav(args.m3u8_url, args.output_name)
+    transcribed_text = convert_speech_to_text(f"{args.output_name}.wav")
+    shortend_text = process_text_file(transcribed_text)
+    summarize_text(shortend_text, f'{args.output_name}.txt')
 
 
 if __name__ == "__main__":
+    dotenv_path = join(dirname(__file__), '../../.env.local')
+    load_dotenv(dotenv_path)
+
     main()
